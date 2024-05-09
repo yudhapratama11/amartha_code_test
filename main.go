8d717958808efc0796afee757eb4d7d881ab7b4d
yudhapratama10 <yudha.pratama1398@gmail.com>
2024-05-10 01:04:33 +0700

    refactor: remove unused code

--- main.go
+++ main.go
@@ -9,28 +9,6 @@ import (
 	usecases "yudhapratama11/amartha_code_test/usecase"
 )
 
-type Loan struct {
-	ID              int       `json:"id"`
-	Principal       float64   `json:"principal"`
-	WeeklyPayment   float64   `json:"weekly_payment"`
-	InterestRate    float64   `json:"interest_rate"`
-	WeeksPayment    []float64 `json:"weeks_payment"`
-	Status          string    `json:"status"`
-	InstallmentTerm int       `json:"installment_term"`
-	StartDate       time.Time `json:"start_date"`
-}
-
-func (l *Loan) checkTotalUnpaid(weeksElapsed int) int {
-	ctrNonPaid := 0
-	for i := 0; i < weeksElapsed; i++ {
-		if l.WeeksPayment[i] == 0 {
-			ctrNonPaid++
-		}
-	}
-
-	return ctrNonPaid
-}
-
 func main() {
 	loanRepo := repository.NewLoanRepository()
 	loanUsecase := usecases.NewLoanUsecase(loanRepo)
